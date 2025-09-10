b5cbad8b9ae01e73e5782232f459e7c08b2d3309
mohamed salah <mohamed0salah213@gmail.com>
2025-09-10 12:05:07 +0300

    update now changes saved in the database

--- profile.php
+++ profile.php
@@ -1,8 +1,17 @@
 <?php
 session_start();
 
+require_once 'database_connection.php';
+
 if (isset($_POST['edit'])) {
-    $_SESSION['username'] = $_POST['username'];
+    $new_username = $_POST['username'];
+    $new_password = $_POST['password'];
+    $old_username = $_SESSION['username'];
+
+    $sql = "UPDATE users SET username='$new_username', password='$new_password' WHERE username='$old_username'";
+    mysqli_query($conn, $sql);
+
+    $_SESSION['username'] = $new_username;
 }
 
 if (isset($_POST['logout'])) {
@@ -17,6 +26,10 @@ if (isset($_POST['logout'])) {
     <form method="post" style="text-align: center;">
         <label>Username:</label>
         <input type="text" name="username" value="<?php echo $_SESSION['username']; ?>">
+        <br><br>
+        <label>Password:</label>
+        <input type="text" name="password" value="">
+        <br><br>
         <input type="submit" name="edit" value="Edit">
     </form>
     <form method="post" style="margin-top:10px;">
